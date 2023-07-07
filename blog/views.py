1434d2d48e6fc00e947563fbe51f9124107de799
Abrar <abrar.ali75@gmail.com>
2023-07-07 18:56:30 +0530

    added user profile

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
+def about(request):
+    return render(request, 'blog/about.html', context={"title": "About django"})
+
+@login_required
 def home(request):
     context = {
         "posts": Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
 
-def about(request):
-    return render(request, 'blog/about.html', context={"title": "About django"})
+
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -8,11 +9,15 @@ def register(request):
           if form.is_valid():
                form.save()
                username = form.cleaned_data.get('username')
-               messages.success(request, f'Account created for {username}')
-               return redirect('blog-home')
+               messages.success(request, 'Your account has been created, you can now Login!')
+               return redirect('login') #returns to the login view once the user has been registered
      else:     
         form = UserRegisterForm()
      context = {
           'form': form
      }
      return render(request, 'users/register.html', context)
+
+@login_required #this makes sure that a user is logged in to view the profile view
+def profile(request):
+     return render(request, 'users/profile.html')

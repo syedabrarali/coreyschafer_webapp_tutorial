9ca3b96b0a87a5ec21b25d1b63ae1d27a7f50f6c
Syed Abrar Ali <abrar@Syeds-MacBook-Air.local>
2023-07-12 17:52:26 +0530

    added profile and user update functionality and added profile picture to posts

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self): #changing the way the save method works 
+        super().save()
+
+        #resizing the image uploaded
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path) 
